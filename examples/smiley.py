ea3c0a7a870540328303ee1e73cdc17c9504ca68
Elise Johnson <elise.e.johnson11@gmail.com>
2021-10-31 23:26:57 -0600

    add smiley face

---
+++ examples/smiley.py
@@ -0,0 +1,31 @@
+import turtle
+
+stella = turtle.Turtle()
+
+stella.circle(100)
+
+stella.left(90)
+stella.penup()
+stella.forward(150)
+
+stella.left(90)
+stella.forward(30)
+stella.pendown()
+stella.circle(15)
+stella.penup()
+stella.back(60)
+stella.pendown()
+stella.circle(15)
+stella.penup()
+stella.forward(30)
+stella.left(90)
+stella.forward(50)
+stella.left(90)
+stella.forward(75)
+stella.left(90)
+
+stella.pendown()
+stella.circle(75, -180)
+
+
+input()
